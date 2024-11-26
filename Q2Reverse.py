6b7c77d468efb93af5f60b0add83937d7ac22939
jack-saunders-0 <jack.saunders@stu.ocsb.ca>
2024-11-26 10:23:51 -0500

    Update Q2Reverse.py

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,8 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+def main():
+    word = input("Enter a word: ")
+    for letter in reversed(word):
+        print(letter)
+
+if __name__ == "__main__":
+    main()
